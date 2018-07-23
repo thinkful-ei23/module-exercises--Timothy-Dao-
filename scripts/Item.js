e6c97a848043230f1d01da8f1224325898196e95
Antoine Catt <antoinecatt@gmail.com>
2018-07-23 08:40:08 -0700

    added Item.js and validateName function

---
+++ scripts/Item.js
@@ -0,0 +1,26 @@
+/* global $, cuid */
+
+
+const Item = (function(){
+  
+  const validateName = name => {
+    if(name === '') {
+     throw new TypeError("Name doesnt exist");
+    };
+  }
+
+  const create = name => {
+    return {
+      id: cuid(),
+      name: name,
+      checked: false
+    };
+  };
+
+  return {
+    validateName: validateName,
+    create: create
+  }
+
+}()
+);
